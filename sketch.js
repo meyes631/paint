d4a8f548352c360cf708d9b74ed042c6bb064cfb
meyes631 <meyes631@gmail.com>
2015-11-06 01:44:07 -0500

    myownphotoshop

--- sketch.js
+++ sketch.js
@@ -7,15 +7,18 @@ var colorPicker;
 var slider;
 var clearButton;
 var saveButton;
+var clearButton
 var brushPicker;
 var brushType;
+var paintBucket
 
-var pumpkin;
+var apple,chromy;
 
 
 
 function preload(){
-    pumpkin = loadImage("images/pumpkin.png");
+    apple = loadImage("images/apple.png");
+    chromy = loadImage("images/chromy.png");
     imageMode(CENTER);
 }
 
@@ -38,12 +41,21 @@ function setup() {
     saveButton = select('.saveButton');
     saveButton.mouseClicked(saveFunction);
 
+    clearButton = select('.clearButton');
+    clearButton.mouseClicked(clearFunction);
+
     //set up the brush type
     brushPicker = createSelect();
     brushPicker.parent("brushType")
     brushPicker.option('paint brush');
     brushPicker.option('spray can');
-    brushPicker.option('image');
+
+    brushPicker.option('apple');
+    brushPicker.option('triangle');
+    brushPicker.option('chromy');
+    brushPicker.option('paint bucket');
+    brushPicker.option('eraser');
+
     brushPicker.changed(changeBrush);
     brushType = brushPicker.value();
 }
@@ -55,14 +67,22 @@ function draw() {
             sprayCan();
         } else if(brushType == "paint brush"){
             standardStroke(); 
-        } else if(brushType == "image"){
-            drawImage(); 
+        } else if(brushType == "apple"){
+            drawApple(); 
+        } else if(brushType == "triangle"){
+            triangleStroke(); 
+        } else if(brushType == "chromy"){
+            drawChromy(); 
+        } else if(brushType == "paint bucket"){
+            paintBucket(); 
+        } else if(brushType == "eraser"){
+            eraserBrush(); 
         }
         
     } else {
         //Cursor options: ARROW, CROSS, HAND, MOVE, TEXT, or WAIT, or path for image
         //if you use an image, the recommended size is 16x16 or 32x32 pixels
-        cursor(CROSS);
+        cursor(HAND);
     }
 }
 
@@ -88,6 +108,25 @@ function standardStroke(){
 
 }
 
+function triangleStroke(){
+    //set the size of the brush from the slider
+    strokeWeight(slider.value());
+    stroke("#"+colorPicker.value());
+
+    //use the hex code for the stroke color
+    // triangle("#"+colorPicker.value());
+    //If you want to use the RGB values instead you can do so like this:
+    //(useful if you want to add opacity with RGBA)
+    // stroke(colorPicker.elt.color.rgb[0]*255, 
+    //         colorPicker.elt.color.rgb[1]*255, 
+    //         colorPicker.elt.color.rgb[2]*255
+    //         );
+
+    //pmouseX and pmouseY give you the previous mouse position
+    triangle(mouseX, mouseY, mouseX+-40, mouseY+60, mouseX+40, mouseY+60);
+
+}
+
 function sprayCan(){
     var sliderVal = slider.value();
     stroke( "#"+colorPicker.value() );
@@ -102,9 +141,14 @@ function sprayCan(){
     }    
 }
 
-function drawImage(){
+function drawApple(){
     //draw the image where the mouse is and set the size to the brush size
-    image(pumpkin,mouseX,mouseY, slider.value(), slider.value());
+    image(apple,mouseX,mouseY, slider.value(), slider.value());
+}
+
+function drawChromy(){
+    //draw the image where the mouse is and set the size to the brush size
+    image(chromy,mouseX,mouseY, slider.value(), slider.value());
 }
 
 //--------------------------
@@ -115,7 +159,22 @@ function changeBrush(){
     brushType = brushPicker.value();
 }
 
-
 function saveFunction() {
     save(drawingCanvas, "myDrawing.jpg");
+}
+
+function clearFunction() {
+    clear();
+    background("white");
+}
+
+function paintBucket() {
+    background("#"+colorPicker.value());
+}
+
+function eraserBrush(){
+    strokeWeight(slider.value());
+    stroke("white");
+
+    line(pmouseX, pmouseY, mouseX, mouseY);
 }
